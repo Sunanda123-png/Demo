93cee0cb49c8f8abd909ca84f6473e5da90bacd2
Sunanda123-png <sunanda02@gmail.com>
2021-08-06 09:52:37 +0530

    addding world in print

--- src/com/sunanda/demo/HelloWorld.java
+++ src/com/sunanda/demo/HelloWorld.java
@@ -3,6 +3,6 @@ package com.sunanda.demo;
 public class HelloWorld {
     public static void main(String[] args)
     {
-        System.out.println("Hello Welcome to java");
+        System.out.println("Hello Welcome to javaworld");
     }
 }
